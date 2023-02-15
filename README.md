b94b0c71d710d1e0eecbef95f42d2be730534bfd
romeldris <romel.dris@gmail.com>
2023-02-15 00:20:31 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# create-typescript-express-rd
+
+Based off [https://blog.logrocket.com/how-to-set-up-node-typescript-express/](Log Rocket's How to set up TypeScript with Node.js and Express)
---
+++ index.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT
+
+app.use(morgan('combined'))
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello from my server :d')
+})
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
