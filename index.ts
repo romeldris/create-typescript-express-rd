97fc5cb901e761a8f7c5cd3ea60c392beb68c7a4
romeldris <romel.dris@gmail.com>
2023-02-15 00:46:02 -0800

    add default port

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import morgan from 'morgan'
 dotenv.config()
 
 const app: Express = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(morgan('combined'))
 app.use(cors())
